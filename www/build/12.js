8a37fe083c521bfbc423e110b26d5c04a287e3db
Murilo Alves <murilo980ti@outlook.com>
2018-06-20 12:02:03 -0300

    quase 2,2

--- www/build/12.js
+++ www/build/12.js
@@ -1,6 +1,6 @@
 webpackJsonp([12],{
 
-/***/ 346:
+/***/ 345:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- www/build/15.js
+++ www/build/15.js
@@ -1,6 +1,6 @@
 webpackJsonp([15],{
 
-/***/ 343:
+/***/ 347:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
